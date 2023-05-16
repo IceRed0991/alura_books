ef94e44cf4f2e1a9c4723d5764c1a2b7bee9a5b0
IceRed0991 <wernergutschow@hotmail.com>
2023-05-16 17:28:02 -0300

    Implementando Filter

---
+++ app/metodoFilter.js
@@ -0,0 +1,10 @@
+const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
+
+btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros)
+
+
+function filtrarLivros(){
+    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+    console.table(livrosFiltrados)
+    exibirOsLivrosNaTela(livrosFiltrados);
+}
